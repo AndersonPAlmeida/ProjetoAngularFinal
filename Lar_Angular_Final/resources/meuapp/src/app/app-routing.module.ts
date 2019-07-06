ad442545008e7ebc52fb3e2b3c31f2514af60ae9
AndersonPAlmeida <andersonpa10pa@gmail.com>
2019-07-06 18:28:21 -0300

    Rotas do Login e do Home

--- Lar_Angular_Final/resources/meuapp/src/app/app-routing.module.ts
+++ Lar_Angular_Final/resources/meuapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FormLoginComponent } from './form-login/form-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: FormLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Lar_Angular_Final/resources/meuapp/src/app/app.module.ts
+++ Lar_Angular_Final/resources/meuapp/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
 import { FormLoginComponent } from './form-login/form-login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavComponent,
-    FormLoginComponent
+    FormLoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
